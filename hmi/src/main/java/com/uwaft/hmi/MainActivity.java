2d628655ae72aea283850cab436888a5f7faeeb6
Benjamin Shepherd <benshep@me.com>
2014-02-05 19:28:17 -0500

    Added Control and Data Fragments

--- hmi/src/main/java/com/uwaft/hmi/MainActivity.java
+++ hmi/src/main/java/com/uwaft/hmi/MainActivity.java
@@ -68,10 +68,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
             // the adapter. Also specify this Activity object, which implements
             // the TabListener interface, as the callback (listener) for when
             // this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
+            actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
         }
     }
 
@@ -125,13 +122,23 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return PlaceholderFragment.newInstance(position + 1);
+                case 1:
+                    return ControlFragment.newInstance(position + 1);
+                case 2:
+                    return DataFragment.newInstance(position + 1);
+            }
+            return null;
+
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
             return 3;
+
         }
 
         @Override
@@ -184,37 +191,79 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         }
     }
 
-    public static class ControlFragment extends Fragment {
+    /**
+     * Controls Fragment
+     */
+
+        public static class ControlFragment extends Fragment {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private static final String ARG_SECTION_NUMBER = "section_number";
+
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            public static ControlFragment newInstance(int sectionNumber) {
+                ControlFragment fragment = new ControlFragment();
+                Bundle args = new Bundle();
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+                fragment.setArguments(args);
+                return fragment;
+            }
+
+            public ControlFragment() {
+            }
+
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                     Bundle savedInstanceState) {
+                View rootView = inflater.inflate(R.layout.fragment_control, container, false);
+                /**TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+                 **/
+                return rootView;
+            }
+        }
+
         /**
-         * The fragment argument representing the section number for this
-         * fragment.
+         * Data Fragment
          */
-        private static final String ARG_SECTION_NUMBER = "Controls";
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static ControlFragment newInstance(int sectionNumber) {
-            ControlFragment fragment = new ControlFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+        public static class DataFragment extends Fragment {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public ControlFragment() {
-        }
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            public static DataFragment newInstance(int sectionNumber) {
+                DataFragment fragment = new DataFragment();
+                Bundle args = new Bundle();
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+                fragment.setArguments(args);
+                return fragment;
+            }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_control, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
+            public DataFragment() {
+            }
+
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                     Bundle savedInstanceState) {
+                View rootView = inflater.inflate(R.layout.fragment_data, container, false);
+                /**TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+                 **/
+                return rootView;
+            }
         }
-    }
 
 
 }
